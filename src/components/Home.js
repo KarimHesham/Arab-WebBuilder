4bf47d031612f37a82bcf9ff122118e4a2630eeb
Kareem Hesham <karimdeveloper@outlook.com>
2022-08-04 19:26:40 +0200

    added shadow to demo page container

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="flex text-blue-400 items-center justify-center m-auto h-screen text-xl">
-      <div className="flex items-center justify-center flex-col w-1/2 p-2 h-1/2 border-slate-800 border rounded-md space-y-3">
+      <div className="flex items-center justify-center flex-col w-1/2 p-2 h-1/2 border-slate-800 border rounded-md space-y-3 shadow-lg">
         <a href="/home">Landing</a>
         <a href="/demo">Editor</a>
         <a href="/workspaces">Workspaces</a>
