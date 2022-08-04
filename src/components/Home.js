cf6370788396e19c747ec079516c616fd4629821
Kareem Hesham <karimdeveloper@outlook.com>
2022-08-04 19:22:51 +0200

    added demo home page for navigation

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex flex-col text-blue-400 items-center justify-center mx-auto">
-      <a href="/home">Landing</a>
-      <a href="/demo">Editor</a>
-      <a href="/workspaces">Workspaces</a>
-      <a href="/workspace">Workspace</a>
-      <a href="/pages">Pages</a>
+    <div className="flex text-blue-400 items-center justify-center m-auto h-screen text-xl">
+      <div className="flex items-center justify-center flex-col w-1/2 p-2 h-1/2 border-slate-800 border rounded-md space-y-3">
+        <a href="/home">Landing</a>
+        <a href="/demo">Editor</a>
+        <a href="/workspaces">Workspaces</a>
+        <a href="/workspace">Workspace</a>
+        <a href="/pages">Pages</a>
+      </div>
     </div>
   );
 };
