0de79ba96e9241a00e93078ae4c7ebb4fb806b5f
Kareem Hesham <karimdeveloper@outlook.com>
2022-08-07 17:55:39 +0200

    added Navbar - Pages section of Sidebar in Editor feature

--- src/features/editor/components/Editor.js
+++ src/features/editor/components/Editor.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from "react";
 import grapesjs from "grapesjs";
+import gjsBlocksBasic from "grapesjs-blocks-basic";
+import { Navbar } from "../../../components";
+import { PencilIcon, PlusIcon, TrashIcon } from "@heroicons/react/solid";
 
 const Editor = () => {
   const [editor, setEditor] = useState(null);
@@ -7,12 +10,70 @@ const Editor = () => {
   useEffect(() => {
     const editor = grapesjs.init({
       container: "#editor",
+      plugins: [gjsBlocksBasic],
+      pluginsOpts: {
+        gjsBlocksBasic: {},
+      },
+      blockManager: {
+        appendTo: "#blocks",
+      },
     });
 
     setEditor(editor);
   }, []);
 
-  return <div id="editor"></div>;
+  return (
+    <>
+      <Navbar />
+      <div className="fixed left-0 w-48 md:w-64 h-screen overflow-scroll overflow-x-hidden bg-white border  shadow-2xl rounded-md -z-10">
+        {/* <div className="flex items-center justify-center">
+          <img
+            src="http://arabcode.ae/images/arab-logo.png"
+            alt=""
+            className="w-1/3"
+          ></img>
+        </div> */}
+
+        <button className="flex items-center bg-blue-600 hover:bg-blue-700 px-4 py-1 rounded-md my-6 justify-center text-white w-10/12 mx-auto">
+          <PlusIcon className="h-4" />
+          Add Page
+        </button>
+
+        <div className="flex items-center">
+          <ul className="w-full overflow-x-auto overflow-y-scroll scroll-smooth h-40 border-b-2 border-slate-200">
+            <li className="flex items-center justify-between shadow-sm px-4 py-2 hover:scale-105 hover:bg-slate-100">
+              Page1
+              <div className="flex items-center">
+                <PencilIcon className="h-5 text-blue-600 hover:scale-125" />
+                <TrashIcon className="h-5 text-red-500 hover:scale-125" />
+              </div>
+            </li>
+            <li className="flex items-center justify-between px-4 py-2 shadow-sm rounded-md hover:scale-105 hover:bg-slate-100">
+              Page1
+              <div className="flex items-center">
+                <PencilIcon className="h-5 text-blue-600 hover:scale-125" />
+                <TrashIcon className="h-5 text-red-500 hover:scale-125" />
+              </div>
+            </li>
+            <li className="flex items-center justify-between px-4 py-2 shadow-sm rounded-md hover:scale-105 hover:bg-slate-100">
+              Page1
+              <div className="flex items-center">
+                <PencilIcon className="h-5 text-blue-600 hover:scale-125" />
+                <TrashIcon className="h-5 text-red-500 hover:scale-125" />
+              </div>
+            </li>
+            <li className="flex items-center justify-between px-4 py-2 shadow-sm rounded-md hover:scale-105 hover:bg-slate-100">
+              Page1
+              <div className="flex items-center">
+                <PencilIcon className="h-5 text-blue-600 hover:scale-125" />
+                <TrashIcon className="h-5 text-red-500 hover:scale-125" />
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Editor;
---
+++ src/features/editor/components/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div id="navbar" className="w-full h-6 bg-white shadow-md"></div>;
+};
+
+export default Navbar;
