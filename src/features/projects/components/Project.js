86e3759ba839ab2ac70aa5ef124cd880d7a15a62
Kareem Hesham <karimdeveloper@outlook.com>
2022-08-14 07:22:51 +0200

    added Add Project modal in projects feature

--- src/features/projects/components/Project.js
+++ src/features/projects/components/Project.js
@@ -1,25 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { Project, Navbar, Sidebar } from "../../../components";
 import ConstructionIcon from "@mui/icons-material/Construction";
+import AddIcon from "@mui/icons-material/Add";
 
 const workspace = require("../../../data/workspace.json");
 
 const Workspace = () => {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div>
       <Navbar />
       <div className="flex px-4 md:px-36">
         <Sidebar />
 
-        <div className="mt-10 space-y-4">
-          <div className="flex space-x-1 items-center justify-start">
-            <ConstructionIcon className="h-4" />
-            <h2 className="font-bold text-gray-600">Your Projects</h2>
+        <div className="mt-10 space-y-4 w-full">
+          <div className="flex items-center justify-between md:pr-32">
+            <div className="flex space-x-1 items-center justify-start">
+              <ConstructionIcon className="h-4" />
+              <h2 className="font-bold text-gray-600">Your Projects</h2>
+            </div>
+
+            <div
+              onClick={() => setShowModal(true)}
+              className="flex items-center w-16 h-6 md:w-24 md:h-8 text-xs md:text-sm bg-blue-600 rounded-md justify-center text-white hover:bg-blue-700 cursor-pointer font-semibold"
+            >
+              <AddIcon className="h-4" />
+              <p>New</p>
+            </div>
           </div>
+
           <div className="flex flex-wrap">
             {workspace.projects.map((project) => {
               return <Project key={project.id} name={project.name} />;
             })}
+          </div>
+          <div className="flex items-center justify-center z-50">
+            {showModal ? (
+              <div className="w-64 h-32 rounded-md bg-gray border-2 p-2 flex flex-col justify-between shadow-md">
+                <div className="space-y-1">
+                  <h2 className="font-bold">Add Project</h2>
+                  <hr />
+                </div>
+
+                <input
+                  className="border-2 border-gray-400 rounded-md px-1 w-full bg-white"
+                  placeholder="Enter project name"
+                  type="text"
+                />
+
+                <div className="flex space-x-1 justify-end">
+                  <button
+                    onClick={() => setShowModal(false)}
+                    className="w-16 h-6 text-xs md:text-sm bg-gray-400 rounded-md justify-center text-white hover:bg-gray-500 cursor-pointer font-semibold"
+                  >
+                    Cancel
+                  </button>
+                  <button
+                    onClick={() => setShowModal(false)}
+                    className="w-16 h-6 text-xs md:text-sm bg-blue-600 rounded-md justify-center text-white hover:bg-blue-700 cursor-pointer font-semibold"
+                  >
+                    Add
+                  </button>
+                </div>
+              </div>
+            ) : null}
           </div>
         </div>
       </div>
