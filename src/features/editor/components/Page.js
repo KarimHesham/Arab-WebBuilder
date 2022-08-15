f496c8c80f949b37dec13f763e97d14e36055343
Kareem Hesham <karimdeveloper@outlook.com>
2022-08-15 18:15:54 +0200

    added Add Page Modal in Editor Sidebar

--- src/features/editor/components/Page.js
+++ src/features/editor/components/Page.js
@@ -2,10 +2,10 @@ import React from "react";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 
-const Page = () => {
+const Page = ({ id, name }) => {
   return (
     <li className="flex items-center justify-between shadow-sm px-4 py-2 hover:scale-105 hover:bg-slate-100">
-      Page1
+      {name}
       <div className="flex items-center">
         <button>
           <EditIcon className="scale-75 text-blue-600 hover:scale-100" />
--- src/features/editor/components/Sidebar.js
+++ src/features/editor/components/Sidebar.js
@@ -1,24 +1,79 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Page from "./Page";
 import TabsRender from "./Tabs";
 import AddIcon from "@mui/icons-material/Add";
 
+const project = require("../../../data/project.json");
+
 const Sidebar = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [counter, setCounter] = useState(2);
+
+  const [newPage, setNewPage] = useState({});
+
+  useEffect(() => {
+    console.log(project.pages);
+  }, []);
+
+  const addPage = (page) => {
+    project.pages.push(page);
+    setCounter(counter + 1);
+    setShowModal(false);
+  };
+
   return (
     <div className="w-48 md:w-64 h-screen overflow-y-scroll overflow-x-hidden bg-gray-100 border-r-2 rounded-md">
-      <button className="flex items-center bg-blue-600 hover:bg-blue-700 px-4 py-1 rounded-md my-6 justify-center text-white w-10/12 mx-auto">
+      <button
+        onClick={() => setShowModal(true)}
+        className="flex items-center bg-blue-600 hover:bg-blue-700 px-4 py-1 rounded-md my-6 justify-center text-white w-10/12 mx-auto"
+      >
         <AddIcon className="h-4" />
         Add Page
       </button>
 
+      <div className="flex items-center justify-center z-50">
+        {showModal ? (
+          <div className="w-64 h-32 rounded-md bg-gray border-2 p-2 flex flex-col justify-between shadow-md">
+            <div className="space-y-1">
+              <h2 className="font-bold">Add Page</h2>
+              <hr />
+            </div>
+
+            <input
+              className="border-2 border-gray-400 rounded-md px-1 w-full bg-white"
+              placeholder="Enter page name"
+              type="text"
+              onChange={(e) =>
+                setNewPage({
+                  id: counter,
+                  name: e.target.value,
+                  createdAt: new Date().toLocaleDateString(),
+                })
+              }
+            />
+
+            <div className="flex space-x-1 justify-end">
+              <button
+                onClick={() => setShowModal(false)}
+                className="w-16 h-6 text-xs md:text-sm bg-gray-400 rounded-md justify-center text-white hover:bg-gray-500 cursor-pointer font-semibold"
+              >
+                Cancel
+              </button>
+              <button
+                onClick={() => addPage(newPage)}
+                className="w-16 h-6 text-xs md:text-sm bg-blue-600 rounded-md justify-center text-white hover:bg-blue-700 cursor-pointer font-semibold"
+              >
+                Add
+              </button>
+            </div>
+          </div>
+        ) : null}
+      </div>
       <div className="flex items-center border-b-2 border-t-2 overflow-x-hidden overflow-y-scroll scroll-smooth">
         <ul className="w-full h-40 border-slate-200">
-          <Page />
-          <Page />
-          <Page />
-          <Page />
-          <Page />
-          <Page />
+          {project.pages.map((page) => {
+            return <Page id={page.id} name={page.name} />;
+          })}
         </ul>
       </div>
 
